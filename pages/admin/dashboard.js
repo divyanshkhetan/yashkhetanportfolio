99f847fd883c0aa175ee18f51c51fff4aed45726
divyanshkhetan <divyanshkhetan18@gmail.com>
2022-06-08 03:28:40 +0530

    minor improvements

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -3,39 +3,47 @@ import styles from "./dashboard.module.css";
 import Link from "next/link";
 
 export default function dashboard() {
-    return (
-      <>
-        <div className={styles.navbar}>
-            <div className={styles.navbar__item}>
-                <h1 className={styles.dashboardBanner}><a href="/admin/dashboard">Dashboard</a></h1>
-                <div className={styles.btnSection}>
-                  <button className={styles.btn}>Create Project</button>
-                  <Link href="/admin/changePassword"><button className={styles.btn}>Change Password</button></Link>
-                  <button className={styles.signOutBtn} onClick={() => signOut()}>Sign out</button>
-                </div>
-            </div>
+  return (
+    <>
+      <div className={styles.navbar}>
+        <div className={styles.navbar__item}>
+          <h1 className={styles.dashboardBanner}>
+            <Link href="/admin/dashboard">
+              <a>Dashboard</a>
+            </Link>
+          </h1>
+          <div className={styles.btnSection}>
+            <button className={styles.btn}>Create Project</button>
+            <Link href="/admin/changePassword">
+              <button className={styles.btn}>Change Password</button>
+            </Link>
+            <button className={styles.signOutBtn} onClick={() => signOut()}>
+              Sign out
+            </button>
+          </div>
         </div>
-        <div className={styles.container}>
-          <p>
-              This is the dashboard page. You can use this page to display some
-              information about your application.
-          </p>
-        </div>
-      </>
-    );
+      </div>
+      <div className={styles.container}>
+        <p>
+          This is the dashboard page. You can use this page to display some
+          information about your application.
+        </p>
+      </div>
+    </>
+  );
 }
 
 export async function getServerSideProps(context) {
-    const session = await getSession({ req: context.req });
-  
-    if (session) {
-      return { props: { session } };
-    } else {
-      return {
-        redirect: {
-          destination: "/admin",
-          permanent: false,
-        },
-      };
-    }
+  const session = await getSession({ req: context.req });
+
+  if (session) {
+    return { props: { session } };
+  } else {
+    return {
+      redirect: {
+        destination: "/admin",
+        permanent: false,
+      },
+    };
   }
+}
